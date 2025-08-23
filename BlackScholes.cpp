da4692021c444c73cba82f1fdcac4ace34918d56
Ethan Denning <96307838+SubicYt@users.noreply.github.com>
2025-08-22 21:48:09 -0400

    Create BlackScholes.cpp
    First part of implementation. Calculate call price and put price. Greeks next. --> Monte Carlo simulation last.

---
+++ BlackScholes.cpp
@@ -0,0 +1,90 @@
+#define _USE_MATH_DEFINES
+#include<iostream>
+#include<cmath>
+#include<fstream>
+
+struct Contract {
+	float time_to_mature;
+	float strike_price;
+	float current_price;
+	float volatility;
+	float risk_free_int;
+
+	Contract(float ttm, float sp, float current, float vol, float risk) {
+		time_to_mature = ttm;
+		strike_price = sp;
+		current_price = current;
+		volatility = vol;
+		risk_free_int = risk;
+	}
+
+};
+
+struct Greeks {
+	float delta;
+	float gamma;
+	float vega;
+	float theta;
+	float rho;
+};
+
+//standard norm. probability density function
+double norm_pdf(const double& x) {
+	return (1.0 / (pow(2 * M_PI, 0.5))) * exp(-0.5 * x * x);
+}
+
+//approx. of the normal cumulative distribution function. 
+double norm_cdf(const double& x) {
+	double k = 1.0 / (1.0 + 0.2316419 * x);
+	double k_sum = k * (0.319381530 + k * (-0.356563782 + k * (1.781477937 + k * (-1.821255978 + 1.330274429 * k))));
+
+	if (x >= 0.0) {
+		return (1.0 - (1.0 / (pow(2 * M_PI, 0.5))) * exp(-0.5 * x * x) * k_sum);
+	}
+	else {
+		return 1.0 - norm_cdf(-x);
+	}
+}
+/*
+* float add1 = std::log(params.current_price / params.strike_price);
+	float add2 = (params.risk_free_int + (0.5 * ((std::pow(params.volatility, 2.0)))) * params.time_to_mature);
+	float divisor = params.volatility * (std::sqrt(params.time_to_mature));
+	return d1 = (add1 + add2) / divisor;
+*/
+
+double PriceBlackScholesModel(Contract& params, bool isCallOption) {
+	
+	float d1;
+	float d2;
+	float eRT; //See Call Option if statement for description.
+
+	//Calculate d1 value to be passed To CDF of normal distribution func.
+	float add1 = std::log(params.current_price / params.strike_price);
+	float add2 = (params.risk_free_int + (0.5 * ((std::pow(params.volatility, 2.0)))) * params.time_to_mature);
+	float divisor = params.volatility * (std::sqrt(params.time_to_mature));
+	d1 = (add1 + add2) / divisor;
+
+	//Calculate d2 value to be passed to CDF norm distribution func.
+	d2 = d1 - (params.volatility * (std::sqrt(params.time_to_mature)));
+	
+	//If the option is a call use C = N(d1)St - N(d2)Ke^-rt ---> Call option price. 
+	if (isCallOption) {
+		eRT = std::pow(M_E, (-params.risk_free_int * params.time_to_mature)); // Variable used to represent (e^-rt)
+	    float call_price = norm_cdf(d1) * params.current_price - norm_cdf(d2) * params.strike_price * eRT;
+		return call_price;
+	}
+	else { // compute for put option
+		float put_price = params.strike_price * eRT * norm_cdf(-d2) - params.current_price * norm_cdf(-d1);
+		return put_price;
+	}
+	
+}
+
+Greeks calculateGreeks(const Contract& params, bool isCallOption) {
+	//implement later. 
+}
+
+
+int main() {
+	//implement user inputs and csv file output. 
+}
